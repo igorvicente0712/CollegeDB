c826687e0e8c0f294bc51cc632075a0ec7755284
Yuri Bykoff <160950966+YuriBykoff@users.noreply.github.com>
2024-05-23 12:03:35 -0300

    Update creates.sql

--- sql/creates.sql
+++ sql/creates.sql
@@ -1,109 +1,78 @@
--- Caso alguma pessoa, seja professor ou aluno, queira suas informacoes deletadas, optei por colocar seu id como 0, mas podia ser ON DELETE CASCADE tambem
-
+-- Tabela Departamento
 CREATE TABLE IF NOT EXISTS departamento (
-    id_dept INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_dept SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    id_chefe INT NOT NULL
+    id_chefe INT
 );
 
+-- Tabela Professor
 CREATE TABLE IF NOT EXISTS professor (
-    id_prof INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_prof SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    id_dept INT NOT NULL,
-    CONSTRAINT fk_prof_dept
-        FOREIGN KEY(id_dept)
-            REFERENCES departamento(id_dept)
+    id_dept INT NOT NULL
 );
 
+-- Adiciona a chave estrangeira para o chefe do departamento
 ALTER TABLE departamento 
     ADD CONSTRAINT fk_dept_chefe
         FOREIGN KEY(id_chefe)
-            REFERENCES professor(id_curso);
+            REFERENCES professor(id_prof);
 
+-- Tabela Curso
 CREATE TABLE IF NOT EXISTS curso (
-    id_curso INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_curso SERIAL PRIMARY KEY,
     curso VARCHAR(100) NOT NULL
 );
 
+-- Tabela Disciplina
 CREATE TABLE IF NOT EXISTS disciplina (
     cod_disc VARCHAR(6) PRIMARY KEY,
     nome VARCHAR(100),
-    id_dept INT,
-    CONSTRAINT fk_disc_dept
-        FOREIGN KEY(id_dept)
-            REFERENCES departamento(id_dept)
+    id_dept INT
 );
 
+-- Tabela Matriz Curricular
 CREATE TABLE IF NOT EXISTS matriz_curr (
     id_curso INT NOT NULL,
-    cod_disc VARCHAR(6) NOT NULL,
-    PRIMARY KEY (id_curso, cod_disc),
-    CONSTRAINT fk_mat_curso
-        FOREIGN KEY(id_curso)
-            REFERENCES curso(id_curso),
-    CONSTRAINT fk_mat_disc
-        FOREIGN KEY(cod_disc)
-            REFERENCES disciplina(cod_disc)
+    cod_disc VARCHAR(6) NOT NULL
 );
 
+-- Tabela Grupo TCC
 CREATE TABLE IF NOT EXISTS grupo_tcc (
-    id_grupo INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_grupo SERIAL PRIMARY KEY,
     id_orientador INT DEFAULT 0,
-    status_aprovacao VARCHAR(10), -- analisando, reprovado, aprovado, talvez se fosse usar ENUM fosse melhor?
+    status_aprovacao VARCHAR(10), 
     CONSTRAINT fk_grupo_prof
         FOREIGN KEY(id_orientador)
             REFERENCES professor(id_prof)
             ON DELETE SET DEFAULT 
 );
 
+-- Tabela Aluno
 CREATE TABLE IF NOT EXISTS aluno (
-    id_aluno INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_aluno SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     id_curso INT DEFAULT 0,
-    id_grupo INT DEFAULT NULL, -- Da pra colocar na tabela de grupo_tcc, mas achei melhor assim
-    CONSTRAINT fk_aluno_curso
-        FOREIGN KEY(id_curso)
-            REFERENCES curso(id_curso)
-            ON DELETE SET DEFAULT,
-    CONSTRAINT fk_aluno_grupo
-        FOREIGN KEY(id_grupo)
-            REFERENCES grupo_tcc(id_grupo)
-            ON DELETE SET DEFAULT,
+    id_grupo INT DEFAULT NULL
 );
 
--- Turmas com sua respectiva disciplina e professor que leciona, assim como ano e semestre
+-- Tabela Turma
 CREATE TABLE IF NOT EXISTS turma (
-    id_turma INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_turma SERIAL PRIMARY KEY,
     id_prof INT DEFAULT 0 NOT NULL,
     cod_disc VARCHAR(6) NOT NULL,
     ano INT NOT NULL DEFAULT DATE_PART('year', CURRENT_TIMESTAMP),
-    semestre NUMERIC(1,0) NOT NULL DEFAULT (DIV(DATE_PART('month', CURRENT_TIMESTAMP) - 1, 6) + 1) -- So brincando um pouco
-    limite_faltas INT NOT NULL DEFAULT 0,
-    CONSTRAINT fk_turma_prof
-        FOREIGN KEY(id_prof)
-            REFERENCES professor(id_prof)
-            ON DELETE SET DEFAULT,
-    CONSTRAINT fk_turma_disc
-        FOREIGN KEY(cod_disc)
-            REFERENCES disciplina(cod_disc)
-            ON DELETE SET DEFAULT
+    semestre NUMERIC(1,0) NOT NULL DEFAULT ((DATE_PART('month', CURRENT_TIMESTAMP) - 1) / 6 + 1)::INT, -- So brincando um pouco
+    limite_faltas INT NOT NULL DEFAULT 0
 );
 
--- Inscricao individual dos alunos nas suas turmas de escolha
+-- Tabela Inscricao
 CREATE TABLE IF NOT EXISTS inscricao (
-    id_insc INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_insc SERIAL PRIMARY KEY,
     id_aluno INT DEFAULT 0 NOT NULL,
     id_turma INT DEFAULT 0 NOT NULL,
     faltas INT DEFAULT 0,
-    media NUMERIC(2,1),
-    CONSTRAINT fk_insc_aluno
-        FOREIGN KEY(id_aluno)
-            REFERENCES aluno(id_aluno)
-            ON DELETE SET DEFAULT,
-    CONSTRAINT fk_insc_turma
-        FOREIGN KEY(id_turma)
-            REFERENCES turma(id_turma)
-            ON DELETE SET DEFAULT
+    media NUMERIC(2,1)
 );
